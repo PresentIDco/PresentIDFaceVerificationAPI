077b077bbc04422ca0802e830e018ec21145a9bb
Hosein Hojat Ansari <42301185+hhojatansari@users.noreply.github.com>
2020-04-12 15:57:38 +0430

    Update HiBrainyFaceVerificationAPI.cs

--- CSharp/HiBrainyFaceVerificationAPI.cs
+++ CSharp/HiBrainyFaceVerificationAPI.cs
@@ -1,37 +1,40 @@
-﻿string apiKey;
-
-public HiBrainyFaceVerificationAPI(string apiKey)
+class HiBrainyFaceVerificationAPI
 {
-    this.apiKey = apiKey;
-}
+    string apiKey;
 
-public async Task<FaceVerificationResponseResult> FaceVerificationAsync(string FileName1st, string FileName2nd)
-{
-    var content = new MultipartFormDataContent();
-    var response = new HttpResponseMessage();
-    string apiUrl = "http://api.hibrainy.com/api/v1/Face/FaceVerification";
-    FaceVerificationResponseResult faces = new FaceVerificationResponseResult();
-    try
+    public HiBrainyFaceVerificationAPI(string apiKey)
     {
-        content.Headers.Add("API-Key", apiKey);
-        content.Add(new ByteArrayContent(File.ReadAllBytes(FileName1st)), "photo1", FileName1st.Split('\\').LastOrDefault());
-        content.Add(new ByteArrayContent(File.ReadAllBytes(FileName2nd)), "photo2", FileName2nd.Split('\\').LastOrDefault());
+        this.apiKey = apiKey;
     }
-    catch (Exception ex) { throw new Exception(ex.Message); }
-    try
+
+    public async Task<FaceVerificationResponseResult> FaceVerificationAsync(string FileName1st, string FileName2nd)
     {
-        using (var http = new HttpClient())
+        var content = new MultipartFormDataContent();
+        var response = new HttpResponseMessage();
+        string apiUrl = "http://api.hibrainy.com/api/v1/Face/FaceVerification";
+        FaceVerificationResponseResult faces = new FaceVerificationResponseResult();
+        try
         {
-            response = await http.PostAsync(apiUrl, content);
+            content.Headers.Add("API-Key", apiKey);
+            content.Add(new ByteArrayContent(File.ReadAllBytes(FileName1st)), "photo1", FileName1st.Split('\\').LastOrDefault());
+            content.Add(new ByteArrayContent(File.ReadAllBytes(FileName2nd)), "photo2", FileName2nd.Split('\\').LastOrDefault());
         }
-        string resMessage = await response.Content.ReadAsStringAsync();
-        if (response.IsSuccessStatusCode)
+        catch (Exception ex) { throw new Exception(ex.Message); }
+        try
         {
-            faces = JsonConvert.DeserializeObject<FaceVerificationResponseResult>(resMessage);
-            return faces;
+            using (var http = new HttpClient())
+            {
+                response = await http.PostAsync(apiUrl, content);
+            }
+            string resMessage = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                faces = JsonConvert.DeserializeObject<FaceVerificationResponseResult>(resMessage);
+                return faces;
+            }
+            else
+            { throw new Exception(resMessage); }
         }
-        else
-        { throw new Exception(resMessage); }
+        catch (Exception ex) { throw new Exception(ex.Message); }
     }
-    catch (Exception ex) { throw new Exception(ex.Message); }
 }
