7c52ab55c3b23a905e83b655d8abba1e7c08767b
Peyman27m <63470748+Peyman27m@users.noreply.github.com>
2021-02-06 17:00:03 +0330

    Update FaceVerification.py

--- Python/FaceVerification.py
+++ Python/FaceVerification.py
@@ -1,35 +1,24 @@
 import requests
 
+url = "https://face-verification2.p.rapidapi.com/FaceVerification"
 
-api_url = 'http://api.hibrainy.com/api/v1/Face/FaceVerification'
-api_key = 'Your API-Key'
+payload = "-----011000010111000001101001\r
+Content-Disposition: form-data; name=\"photo1\"\r
+\r
+\r
+-----011000010111000001101001\r
+Content-Disposition: form-data; name=\"photo2\"\r
+\r
+\r
+-----011000010111000001101001--\r
+\r
+"
+headers = {
+    'content-type': "multipart/form-data; boundary=---011000010111000001101001",
+    'x-rapidapi-key': "f485496ab8msh2ea068a0bbe333cp179f86jsn2b4afe8962dd",
+    'x-rapidapi-host': "face-verification2.p.rapidapi.com"
+    }
 
-image1_path = 'Path to image1 directory'
-image1_name = 'Image name1'
-image2_path = 'Path to image2 directory'
-image2_name = 'Image name2'
+response = requests.request("POST", url, data=payload, headers=headers)
 
-files = {'Photo1': (image1_name, open(image1_path + image1_name, 'rb'), 'multipart/form-data'),
-         'Photo2': (image2_name, open(image2_path + image2_name, 'rb'), 'multipart/form-data')}
-header = {'API-Key': api_key}
-response = requests.post(api_url, files=files, headers=header)
-
-print("Status Code:", response.json()['StatusCode'])
-print("Status Message:", response.json()['StatusMessage'])
-print("Has Error:", response.json()['HasError'])
-
-print("\nResult Index:", response.json()['VerificationResult']['resultIndex'])
-print("Result Message:", response.json()['VerificationResult']['resultMessage'])
-print("Similar Percent:", response.json()['VerificationResult']['similarPercent'])
-
-for i in range(0, 2):
-    print('\nFace', i + 1, 'Information:')
-    print('Rectangle')
-    print("LeftTop", " X:", response.json()['Features'][i]['Framepoints']['LeftTop']['x'],
-          " Y:", response.json()['Features'][i]['Framepoints']['LeftTop']['y'])
-    print("LeftBottom", " X:", response.json()['Features'][i]['Framepoints']['LeftBottom']['x'],
-          " Y:", response.json()['Features'][i]['Framepoints']['LeftBottom']['y'])
-    print("RightTop", " X:", response.json()['Features'][i]['Framepoints']['RightTop']['x'],
-          " Y:", response.json()['Features'][i]['Framepoints']['RightTop']['y'])
-    print("RightBottom", " X:", response.json()['Features'][i]['Framepoints']['RightBottom']['x'],
-          " Y:", response.json()['Features'][i]['Framepoints']['RightBottom']['y'])
+print(response.text)
